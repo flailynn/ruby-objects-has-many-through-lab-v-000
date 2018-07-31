f5fd674f3b501f0837267e52ac37cf3f9d3bf3c1
Anita Beauchamp <amadamspr@gmail.com>
2018-07-31 04:08:16 +0000

    automatically backed up by learn

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -1,4 +1,12 @@
 class Appointment
+  attr_accessor :patient, :doctor, :date
+  @@all = []
 
+  def initialize(patient, doctor, date)
+    self.patient = patient
+    self.doctor = doctor
+    self.date = date
+    @@all << self
+  end
 
 end
